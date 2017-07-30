bfd0073171b95abf48e6640d18c58a5b5be47171
Hwi Jae, Shin <pkallru@gmail.com>
2017-07-31 00:04:22 +0900

    웹브라우저 테스트 추가

--- SampleAutoLoginWSE/Program.cs
+++ SampleAutoLoginWSE/Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SampleAutoLoginWSE
 {
@@ -12,29 +14,61 @@ namespace SampleAutoLoginWSE
     {
         static void Main(string[] args)
         {
-            String callUrl = "https://iamapi.wallstreetenglish.com/api/connect/token";
+            var thread = new Thread(() =>
+            {
+                var browser = new WebBrowser();
+                browser.Visible = true;
+                browser.ScriptErrorsSuppressed = true;
+                browser.DocumentCompleted += WebBrowserDocumentCompletedEventHandler;
+                browser.Navigate("https://world.wallstreetenglish.com/login");
+                Application.Run();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            //String callUrl = "https://iamapi.wallstreetenglish.com/api/connect/token";
+
+            //String postData = "username=sl-kn.32366.kr&amp;password=Hwijae1234&amp;clientId=WSE-FrontEnd&amp;clientSecret=bnNlLXBhc3N3b3Jk";
+
+            //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(callUrl);
+            //// 인코딩 UTF-8
+            //byte[] sendData = UTF8Encoding.UTF8.GetBytes(postData);
+            //httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            //httpWebRequest.Method = "POST";            
+            //httpWebRequest.ContentLength = sendData.Length;
+            //httpWebRequest.Accept = "application/json, text/plain, */*";
+
+            //Stream requestStream = httpWebRequest.GetRequestStream();
+            //requestStream.Write(sendData, 0, sendData.Length);
+            //requestStream.Close();
+
+            //HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            //StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+            //var aa = streamReader.ReadToEnd();
+            //streamReader.Close();
+            //httpWebResponse.Close();
+
+            //Console.Write("return: " + aa);
+        }
+
+        static public void WebBrowserDocumentCompletedEventHandler(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var browser = sender as WebBrowser;
+
+            HtmlDocument doc = browser.Document;
             
-            String postData = "username=sl-kn.32366.kr&amp;password=Hwijae1234&amp;clientId=WSE-FrontEnd&amp;clientSecret=bnNlLXBhc3N3b3Jk";
+            using (var stream = new StreamWriter("test.txt", false))
+            {
+                stream.Write(doc.Body.OuterHtml);
+            }
 
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(callUrl);
-            // 인코딩 UTF-8
-            byte[] sendData = UTF8Encoding.UTF8.GetBytes(postData);
-            httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            httpWebRequest.Method = "POST";            
-            httpWebRequest.ContentLength = sendData.Length;
-            httpWebRequest.Accept = "application/json, text/plain, */*";
-
-            Stream requestStream = httpWebRequest.GetRequestStream();
-            requestStream.Write(sendData, 0, sendData.Length);
-            requestStream.Close();
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
-            var aa = streamReader.ReadToEnd();
-            streamReader.Close();
-            httpWebResponse.Close();
-
-            Console.Write("return: " + aa);
+            HtmlElement username = doc.GetElementById("login-user-name");
+            HtmlElement password = doc.GetElementById("login-password");
+            HtmlElement submit = doc.GetElementById("login-password");
+            //username.SetAttribute("value", "sl-kn.32366.kr");
+            //password.SetAttribute("value", "Hwijae1234");
+            //submit.InvokeMember("click");
         }
     }
 }
---
+++ test/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+// ******  중요  ***************************************************************************************
+// WebBrowser 객체에서 Javascript를 제대로 지원하려면,
+// 레지스트리에 어플리케이션 실행 파일을 IE 버전과 함께 등록해줘야 한다.
+// 레지스트리 위치 : HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION
+// 값 : 어플이름.exe / DWORD / (10진수)11001 (값 의미 - IE v.11)
+// ****************************************************************************************************
+//
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.DocumentCompleted += WebBrowserDocumentCompletedEventHandler;
+            webBrowser1.StatusTextChanged += StatusTextChanged;
+            //webBrowser1.ProgressChanged += new WebBrowserProgressChangedEventHandler(_ie);
+            //webBrowser1.Navigating += WebBrowser1_Navigating;
+            //webBrowser1.Navigated += WebBrowser1_Navigated;
+            webBrowser1.Navigate("https://world.wallstreetenglish.com/login");
+        }
+
+        private void WebBrowser1_AbsolutelyComplete()
+        {
+            HtmlDocument doc = webBrowser1.Document;
+            
+            HtmlElement submit = doc.GetElementById("login-submit");
+
+            WriteInputById(doc, "login-user-name", "sl-kn.32366.kr");
+            WriteInputById(doc, "login-password", "Hwijae1234");
+            submit.InvokeMember("click");
+        }
+
+        public void StatusTextChanged(object sender, EventArgs e)
+        {
+            if(webBrowser1.ReadyState == WebBrowserReadyState.Complete && webBrowser1.IsBusy == false)
+            {
+                HtmlElement username = webBrowser1.Document.GetElementById("login-user-name");
+                if (username != null)
+                {
+                    WebBrowser1_AbsolutelyComplete();
+                }
+            }
+        }
+
+        public void WebBrowserDocumentCompletedEventHandler(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var browser = sender as WebBrowser;
+
+            var a = browser.ActiveXInstance;
+            var b = webBrowser1.ActiveXInstance;
+
+            bool aa = e.Url.AbsolutePath != browser.Url.AbsolutePath;
+            
+            HtmlDocument doc = browser.Document;
+            
+            HtmlElement username = doc.GetElementById("login-user-name");
+            HtmlElement password = doc.GetElementById("login-password");
+            HtmlElement submit = doc.GetElementById("login-password");
+            //username.SetAttribute("value", "sl-kn.32366.kr");
+            //password.SetAttribute("value", "Hwijae1234");
+            //submit.InvokeMember("click");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HtmlDocument doc = webBrowser1.Document;
+
+            HtmlElement username = doc.GetElementById("login-user-name");
+            HtmlElement password = doc.GetElementById("login-password");
+            HtmlElement submit = doc.GetElementById("login-submit");
+
+            WriteInputById(doc, "login-user-name", "sl-kn.32366.kr");
+            WriteInputById(doc, "login-password", "Hwijae1234");
+            submit.InvokeMember("click");
+        }
+
+        private void WriteInputById(HtmlDocument doc, string id, string value)
+        {
+            string script = string.Format("angular.element(document.querySelector( '#{0}' )).val('{1}').triggerHandler('change')", id, value);
+            doc.InvokeScript("eval", new[] { script });
+        }
+    }
+}
